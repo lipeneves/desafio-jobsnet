645745ce3e4ed93e6caa6c41a535d0dd6dfc8cb2
Felipe Neves <88124495+lipeneves@users.noreply.github.com>
2021-08-29 00:58:21 -0300

    API viaCep

---
+++ js/main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const preencherFormulario = (endereco) => {
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+const limparFormulario = () => {
+    document.getElementById('endereco').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+}
+
+const pesquisarCep = async () => {
+    const cep = document.getElementById("cep").value;
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    const erroCep = document.getElementById("erroCEP");
+
+    const dados = await fetch(url);
+    const endereco = await dados.json();
+
+    if (endereco.hasOwnProperty('erro')) {
+        limparFormulario();
+        erroCep.style.display = "inline-block";
+    } else {
+        preencherFormulario(endereco);
+        erroCep.style.display = "none";
+    }
+}
+
+document.getElementById('cep').addEventListener('focusout', pesquisarCep);
+
