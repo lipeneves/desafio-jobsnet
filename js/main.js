927b9eb0f5777370db388c338c74cab3fac6a445
Felipe Neves <88124495+lipeneves@users.noreply.github.com>
2021-08-29 01:16:40 -0300

    Correção link viaCep

--- js/main.js
+++ js/main.js
@@ -7,15 +7,23 @@ const preencherFormulario = (endereco) => {
     document.getElementById('estado').value = endereco.uf;
 }
 
+const limparFormulario = () => {
+    document.getElementById('endereco').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+}
+
 const pesquisarCep = async () => {
     const cep = document.getElementById("cep").value;
-    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
     const erroCep = document.getElementById("erroCEP");
 
     const dados = await fetch(url);
     const endereco = await dados.json();
 
     if (endereco.hasOwnProperty('erro')) {
+        limparFormulario();
         erroCep.style.display = "inline-block";
     } else {
         preencherFormulario(endereco);
